49eae2162e1b0fce5817f98e558f444deadc02d9
Gabriel Rocha <rochagbriel@gmail.com>
2023-04-27 00:36:05 +0200

    Connected to Atlas MongoDB and Heroku

--- index.js
+++ index.js
@@ -13,7 +13,13 @@ const Models = require('./models.js');
 const Movies = Models.Movie;
 const Users = Models.User;
 
-mongoose.connect('mongodb://localhost:27017/cfDB', {
+// LOCAL DB ADRESS
+// mongoose.connect('mongodb://localhost:27017/cfDB', {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+// });
+
+mongoose.connect( process.env.CONNECTION_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
